eddbba8e8d77cd037fee4512bf092395e017e311
Androsz11 <73430696+Androsz11@users.noreply.github.com>
2021-06-25 16:16:51 +0530

    Add files via upload

---
+++ Rock_Paper_Scissor.py
@@ -0,0 +1,84 @@
+# Game: Rock Paper Scissor
+# Developed Date: 24th June 2021
+# Updated on: 25th June 2021
+# Version: 1.0.1
+# Developer: HMandro
+
+import os
+import time
+import random
+
+Loop = True
+
+while Loop == True:
+    choice1 = int(input("Enter your choice: Rock(1),Paper(2),Scissor(3),Exit(4)\n"))
+    choice2 = random.randint(1,3)
+
+    if choice1 == 1 and choice2 == 1:
+        os.system('cls')
+        print("You chose Rock.\nCom chose Rock.\n")
+        print("Tie!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 1 and choice2 == 2:
+        os.system('cls')
+        print("You chose Rock.\nCom chose Paper.\n")
+        print("You Lose!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 1 and choice2 == 3:
+        os.system('cls')
+        print("You chose Rock.\nCom chose Scissor.\n")
+        print("You Win!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 2 and choice2 == 1:
+        os.system('cls')
+        print("You chose Paper.\nCom chose Rock.\n")
+        print("You Win!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 2 and choice2 == 2:
+        os.system('cls')
+        print("You chose Paper.\nCom chose Paper.\n")
+        print("Tie!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 2 and choice2 == 3:
+        os.system('cls')
+        print("You chose Paper.\nCom chose Scissor.\n")
+        print("You Lose!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 3 and choice2 == 1:
+        os.system('cls')
+        print("You chose Scissor.\nCom chose Rock.\n")
+        print("You Lose!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 3 and choice2 == 2:
+        os.system('cls')
+        print("You chose Scissor.\nCom chose Paper.\n")
+        print("You Win!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 3 and choice2 == 3:
+        os.system('cls')
+        print("You chose Scissor.\nCom chose Scissor.\n")
+        print("Tie!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif choice1 == 4:
+        Loop = False
+
+    else:
+        print("Wrong Input!")
